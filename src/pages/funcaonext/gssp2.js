9c53d0de17e1799fd9e705e03c991932ff0bff65
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-06-04 15:45:36 +0000

    add noticias json

--- src/pages/funcaonext/gssp2.js
+++ src/pages/funcaonext/gssp2.js
@@ -1,9 +1,11 @@
-import { Container } from "react-bootstrap"
+import { Container, Table } from "react-bootstrap"
+import { noticias } from "../api/noticias/noticias"
+
 export async function getServerSideProps() {
     return {
         props: {
             repo:
-                { "var1": "Info A", "var2": "FW1","nome":"IFMS" }
+                { "var1": "Info A", "var2": "FW1", "nome": "IFMS" }
         }
     }
 }
@@ -17,6 +19,32 @@ export default function Gssp2({ repo }) {
                 {Object.entries(repo).map(([key, value]) => (
                     <p>{key}: {value}</p>
                 ))}
+            </Container>
+            <Container>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Título</th>
+                            <th>Conteudo</th>
+                            <th>Tipo</th>
+                            <th>DH</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Array.isArray(noticias) ?
+                            noticias.map(noticia => 
+                                <tr>
+                                    <td>{noticia.idnoticia}</td>
+                                    <td>{noticia.titulonoticia}</td>
+                                    <td>{noticia.conteudonoticia}</td>
+                                    <td>{noticia.tiponoticia}</td>
+                                    <td>{new Date(noticia.datahoracadastro).toLocaleString()}</td>
+                                </tr>
+                            )
+                            : "não"}
+                    </tbody>
+                </Table>
             </Container>
         </>
     )
